04c8f0ebba7e1c50ebba87b877f63c23652b86f4
bigbr4in <valentin.bigot@epfl.ch>
2020-10-23 20:21:27 +0200

    Log reg optimized

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -168,7 +168,7 @@ def clean(data):
     data = data[:, ~np.all(data[1:] == data[:-1], axis=0)]
     
     # Mean vector column-wise
-    col_mean = np.nanmean(data, axis=0)
+    col_mean = np.nanmedian(data, axis=0)
 
     # Find indices where Nan appears
     inds = np.where(np.isnan(data))
@@ -208,6 +208,20 @@ def build_poly(x, degree):
     for deg in range(1, degree+1):
         poly = np.c_[poly, np.power(x, deg)]
     return poly
+
+""" --- FUNCTIONS TO OPTIMIZE HYPERPARAMETERS --- """
+
+def build_k_indices(y, k_fold, seed):
+    """build k indices for k-fold."""
+    
+    num_row = y.shape[0]
+    interval = int(num_row / k_fold)
+    np.random.seed(seed)
+    indices = np.random.permutation(num_row)
+    k_indices = [indices[k * interval: (k + 1) * interval]
+                 for k in range(k_fold)]
+    
+    return np.array(k_indices)
 
 """--------- FUNCTIONS TO COMPUTE SPLIT PREDICITONS ---------- """
 
